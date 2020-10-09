cc80ba0aa1e9a000fae7aa09048b3b7ad2352f89
Tailon <tailrusse2020@gmail.com>
2020-10-08 21:13:16 -0800

    Split homework into two projects

---
+++ part a/contigsum.hpp
@@ -0,0 +1,19 @@
+//
+// Created by renew on 10/8/2020.
+//
+
+#ifndef HOMEWORK3_CONTIGSUM_HPP
+#define HOMEWORK3_CONTIGSUM_HPP
+#include <vector>
+#include <deque>
+
+int contigSum(const std::vector<int>::const_iterator& begin, const std::vector<int>::const_iterator& end) {
+    return 0;
+};
+
+int contigSum(std::reverse_iterator<std::deque<int>::const_iterator> begin, std::reverse_iterator<std::deque<int>::const_iterator> end) {
+    return 0;
+};
+
+
+#endif //HOMEWORK3_CONTIGSUM_HPP
